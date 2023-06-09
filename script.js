6cb222d0a85ca15387bd474e61333cc844e7d20e
Yogurt7vgit config --global user.name Yogurt7v <egorov-2k@yandex.rugit config --global user.name Yogurt7v>
2023-06-09 11:35:09 +0300

    done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const lengthSlider = document.querySelector(".pass-length input");
-const options =
-  document.querySelectorAll(".options input"); /* возможно опечатка*/
+const options = document.querySelectorAll(".options input");
 const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
 const passwordIndicate = document.querySelector(".pass-indicator");
